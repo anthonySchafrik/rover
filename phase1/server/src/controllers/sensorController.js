8c5fa43dc0e0357b228c4e3e41b7eef5cfc4041b
Anthony Schafrik <anthonyschafrik@gmail.com>
2020-07-17 18:16:42 -0400

    Server ready for failed api testing

--- phase1/server/src/controllers/sensorController.js
+++ phase1/server/src/controllers/sensorController.js
@@ -11,17 +11,42 @@ const weatherDataPost = async (req, res) => {
     meters,
     uvLight,
     humidity,
+    timecolumn = null,
   } = req.body.weatherData;
 
-  Logger.debug({ temperature, pressure, feet, meters, uvLight, humidity });
+  Logger.debug({
+    temperature,
+    pressure,
+    feet,
+    meters,
+    uvLight,
+    humidity,
+    timecolumn,
+  });
 
   const queryText =
     'INSERT INTO Weather( temperature, pressure, feet, meters, uvindex, humidity) VALUES ($1, $2, $3, $4, $5, $6)';
 
   const queryValue = [temperature, pressure, feet, meters, uvLight, humidity];
 
+  const queryForFailedApi =
+    'INSERT INTO Weather( temperature, pressure, feet, meters, uvindex, humidity, timecolumn) VALUES ($1, $2, $3, $4, $5, $6, $7)';
+
+  const queryValueFailedApi = [
+    temperature,
+    pressure,
+    feet,
+    meters,
+    uvLight,
+    humidity,
+    timecolumn,
+  ];
+
   try {
-    await db.query(queryText, queryValue);
+    await db.query(
+      timecolumn === null ? queryText : queryForFailedApi,
+      timecolumn === null ? queryValue : queryValueFailedApi
+    );
 
     res.sendStatus(200);
   } catch (error) {
